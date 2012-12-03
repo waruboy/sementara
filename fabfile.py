b03cc8d3806fc7af512d309f3d9b4c627c567116
taufiq <waruboy@yahoo.com>
2012-12-03 17:16:29 +0900

    fab, addrepo command

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
 from fabric.api import local
-
+def addrepo():
+	local('heroku git:remote -a diskas-dev')
+	local('git remote add priv https://github.com/balaplumpat/diskas')
 def deploy():
     local('git push heroku master')
     local('heroku run ./manage.py syncdb')
     local('heroku run ./manage.py migrate')
+    local('git push')
